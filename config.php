9816744dc31c130295c9ff42c617f1951d8696a9
Alfonso <9438351+FonzTech@users.noreply.github.com>
2019-10-13 22:18:56 +0200

    Add Config class

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+class Config
+{
+	public static $DB_HOST = "localhost";
+	public static $DB_USERNAME = "root";
+	public static $DB_PASSWORD = "";
+	public static $DB_NAME = "devicelytics";
+}
+?>
